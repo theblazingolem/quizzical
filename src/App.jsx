536c40e48ccffb30de607afb99d2d29b91bbe83f
Blazing <160132339+theblazingolem@users.noreply.github.com>
2025-07-06 20:52:56 +0530

    added state

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+import "./App.css";
+import res from "./response.json";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [quiz, setQuiz] = useState([]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    // fetch(
+    //     "https://opentdb.com/api.php?amount=5&category=11&difficulty=easy&type=multiple"
+    // )
+    //     .then((res) => res.json())
+    //     .then((data) => console.log(datares));
+    // console.log(res.results);
+
+    // console.log(quiz);
+    useEffect(() => {
+        setQuiz(() => res.results);
+    }, []);
+    useEffect(() => {
+        console.log(quiz);
+    }, [quiz]);
+    return (
+        <>
+            <div>
+                <h2>Quizzical</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing</p>
+                <button id="start">Start Game</button>
+            </div>
+        </>
+    );
 }
 
-export default App
+export default App;
