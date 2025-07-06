d519497a0051b4578ac9bb2b9c7d13cee453a9bf
Blazing <160132339+theblazingolem@users.noreply.github.com>
2025-07-06 20:53:30 +0530

    e

---
+++ src/question.jsx
@@ -0,0 +1,8 @@
+export default function Question(props) {
+    props.question;
+    return (
+        <div className="quiz-item">
+            <label className="question"></label>
+        </div>
+    );
+}
